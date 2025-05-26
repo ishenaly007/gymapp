d8ceeba39e7bacc189869d95b46454d6ca9438a7
ishenaly007 <ishennarkozuev07@gmail.com>
2025-05-26 14:58:43 +0600

    asasa

--- src/main/java/com/example/gymapp/config/SecurityConfig.java
+++ src/main/java/com/example/gymapp/config/SecurityConfig.java
@@ -71,7 +71,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); // Разрешаем все источники
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
