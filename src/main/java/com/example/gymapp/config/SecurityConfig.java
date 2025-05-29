bf207cd1b825994437cdaf6ffd279e20271d320c
ishenaly007 <ishennarkozuev07@gmail.com>
2025-05-29 15:31:41 +0600

    add env, change config

--- src/main/java/com/example/gymapp/config/SecurityConfig.java
+++ src/main/java/com/example/gymapp/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://booking-and-scheduling-training-sessions-dyl9.vercel.app", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://booking-and-scheduling-training-sessions-dyl9.vercel.app", "http://localhost:5173", "https://ab1t.top"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
